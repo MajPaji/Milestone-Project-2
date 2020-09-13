0db220ad754fcb9a90145be590b385b11c25c57d
MajPaji <sababi@kth.se>
2020-09-13 03:42:12 +0000

    data added with REST API from (images-api.nasa.gov)

---
+++ assets/js/search.js
@@ -0,0 +1,70 @@
+function imageLoaderHTML(){
+   $(".data").html(`
+   <div id="load-image">
+        <img src="assets/images/load-image.gif" alt="laoding ...">
+   </div>`)
+}
+
+$("#search").keyup(function(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        $("#btnSearch").click();
+    }
+});
+
+$("#btnSearch").click(function() {
+  getImageHTML();  
+  return false;
+});
+
+function getImageHTML(){
+var searchName = $("#search").val();
+console.log(searchName);
+var searchItem = encodeURIComponent(searchName);
+console.log(searchItem);
+
+var xhr = new XMLHttpRequest();
+
+xhr.open("GET", `https://images-api.nasa.gov/search?q=${searchItem}&description=&media_type=image`);
+xhr.send();
+
+xhr.onreadystatechange = function(){
+    if(this.staus = 200 && this.readyState == 4){
+       console.dir(JSON.parse(this.responseText));
+        var data = JSON.parse(this.responseText);
+        /* var dataImage = data.collection.items; */
+        var dataImage = data.collection.items;
+        console.log(dataImage);
+        var imageFiles = [];
+        var imageDescribtion = [];
+       /*  dataImage.forEach(function(item){
+            item.links.forEach(function(item){
+                //console.log(item.href)
+                imageFiles.push(`<img src="${item.href}">`);
+            });
+            item.data.forEach(function(item){
+                console.log(item.title);
+            })
+        }) */
+        console.log(typeof dataImage);
+        dataImage.forEach(function(item){
+            imageFiles.push(`
+            <div class="data-image-article col-10 col-sm-3">
+                        <img src="${item.links[0].href}">
+                        <div class= "data-image-text">
+                        <div class="data-image-title">${item.data[0].title}</div>
+                        <div class="data-image-description">${item.data[0].description}</div>
+                        </div>
+            </div>`)
+        })
+        
+
+    }
+ 
+$(".data").html(`<div class="sky-section container-fluid"><div class="row">${imageFiles}</div><div>`.replace(/,/g, ""));
+$(".data-image-text").hide();   
+}
+
+
+}
+
